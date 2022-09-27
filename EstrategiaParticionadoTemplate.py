8c100d62d8280c58ed80de2d0713bdc9119b2817
Monte-10 <61688940+Monte-10@users.noreply.github.com>
2022-09-27 23:26:22 +0200

    Mejoras

--- EstrategiaParticionadoTemplate.py
+++ EstrategiaParticionadoTemplate.py
@@ -1,9 +1,10 @@
 from abc import ABCMeta,abstractmethod
-
+import random
+import numpy as np
 
 class Particion():
 
-  # Esta clase mantiene la lista de índices de Train y Test para cada partición del conjunto de particiones  
+  # Esta clase mantiene la lista de ï¿½ndices de Train y Test para cada particiï¿½n del conjunto de particiones  
   def __init__(self):
     self.indicesTrain=[]
     self.indicesTest=[]
@@ -14,6 +15,9 @@ class EstrategiaParticionado:
   
   # Clase abstracta
   __metaclass__ = ABCMeta
+
+  def __init__(self):
+    self.particiones = []
   
   # Atributos: deben rellenarse adecuadamente para cada estrategia concreta. Se pasan en el constructor 
   
@@ -27,23 +31,63 @@ class EstrategiaParticionado:
 
 class ValidacionSimple(EstrategiaParticionado):
   
-  # Crea particiones segun el metodo tradicional de division de los datos segun el porcentaje deseado y el número de ejecuciones deseado
+  # Crea particiones segun el metodo tradicional de division de los datos segun el porcentaje deseado y el nï¿½mero de ejecuciones deseado
   # Devuelve una lista de particiones (clase Particion)
-  # TODO: implementar
+
+  #@args pTest porcentaje que se coge para el test
+  #      nEjec numero de ejecuciones
+  def __init__(self, pTest, nEjec):
+      super().__init__()
+      self.pTest = pTest
+      self.nEjec = nEjec
+  
   def creaParticiones(self,datos,seed=None):
-    
     random.seed(seed)
-    pass
+    self.particiones = []
+    longitudDatos = np.shape(datos)[0]
+    longitudTest = int((self.pTest/100)*longitudDatos)
+
+    valores = [i for i in range(longitudDatos)]
+
+    for i in range(self.nEjec):
+      self.particiones.append(Particion())
+
+      random.shuffle(valores)
+
+      self.particiones.append(Particion())
+
+      random.shuffle(valores)
+
+      self.particiones[-1].indicesTest = valores[:longitudTest]
+      self.particiones[-1].indicesTrain = valores[longitudTest:]
       
       
 #####################################################################################################      
 class ValidacionCruzada(EstrategiaParticionado):
-  
+  def __init__(self, numeroParticiones):
+    super().__init__()
+    self.numeroParticiones = numeroParticiones
   # Crea particiones segun el metodo de validacion cruzada.
   # El conjunto de entrenamiento se crea con las nfolds-1 particiones y el de test con la particion restante
   # Esta funcion devuelve una lista de particiones (clase Particion)
-  # TODO: implementar
   def creaParticiones(self,datos,seed=None):   
     random.seed(seed)
-    pass
+    self.particiones = []
+    longitudDatos = np.shape(datos)[0]
+    longitudPorcion = int(longitudDatos/self.numeroParticiones)
+
+    valores = [i for i in range(longitudDatos)]
+    random.shuffle(valores)
+
+    for i in range(self.numeroParticiones):
+      self.particiones.append(Particion())
+
+      #Calculo indices
+      fromTest = i*longitudPorcion
+      toTest = fromTest + longitudPorcion
+
+      #Asigno indices
+      self.particiones[-1].indicesTest = valores[fromTest:toTest]
+      self.particiones[-1].indicesTrain = [i for i in valores if i not in self.particiones[-1].indicesTest]
+    
     
