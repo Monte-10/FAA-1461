53549f1a0f69e11c11c6194f9a51515e74b2944f
Monte-10 <alejandro.monterrubio@estudiante.uam.es>
2022-10-18 11:08:37 +0200

    One Hot Encoder

--- Práctica 1/scikit-learn.py
+++ Práctica 1/scikit-learn.py
@@ -2,21 +2,21 @@
 from sklearn.model_selection import train_test_split, ShuffleSplit
 from sklearn.model_selection import cross_val_score
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, CategoricalNB
+from sklearn.preprocessing import OneHotEncoder
 from sklearn import preprocessing
 import math
+import pandas as pd
 
 from abc import ABCMeta,abstractmethod
 import numpy as np
 from Datos import Datos
 
-dataset = Datos('Práctica 1/ConjuntoDatos/german.csv')
-dataset2 = Datos('Práctica 1/ConjuntoDatos/german.csv')
-#datos = dataset.datos[dataset.datos.keys()[0]:dataset.datos.keys()[-2]]
-#clase = dataset.datos[dataset.datos.keys()[-1]]
-#print(dataset.datos[0])
-
+dataset = Datos('ConjuntoDatos/german.csv')
+dataset2 = Datos('ConjuntoDatos/tic-tac-toe.csv')
+datasetOHE = pd.read_csv('ConjuntoDatos/tic-tac-toe.csv')
 
 atrs = preprocessing.OneHotEncoder()
+
 
 x = atrs.fit_transform(dataset.datos.iloc[:,:-1]).toarray()
 
@@ -116,3 +116,36 @@ print(cross_val_score(clf, x, y, cv=3))
 print("\n\n>>> MULTINOMIAL tic-tac-toe.csv con correcion de laplace..... ")
 clf = MultinomialNB(alpha = 5,fit_prior= True)
 print(cross_val_score(clf, x, y, cv=3))
+
+print('\n\n\n\n\n\n>>>>>>>>>>> TIC TAC TOE -> ONE HOT ENCODER (Validación Simple) <<<<<<<<<<<<<<')
+
+atrs = list(datasetOHE.columns)
+atrs.remove('Class')
+
+ohe = OneHotEncoder()
+
+x = datasetOHE[atrs].values
+y = datasetOHE['Class'].values
+x = ohe.fit_transform(x).toarray()
+
+err_gnb = []
+err_clf = []
+
+gnb = GaussianNB()
+clf = MultinomialNB()
+
+for i in range(0,3):
+    X_train, X_test, Y_train, Y_test = train_test_split(x,y,test_size=0.2,random_state=42)
+    
+    gnb.fit(X_train,Y_train)
+    err_gnb.append(1-gnb.score(X_test,Y_test))
+    
+    clf.fit(X_train, Y_train)
+    err_clf.append(1-clf.score(X_test,Y_test))
+    
+print("\n\n\n>>>---- ERROR GAUSSIAN OHE tic-tac-toe.csv:\n\n")
+for item in err_gnb:
+    print(item)
+print("\n\n\n>>>---- ERROR MULTINOMIAL OHE tic-tac-toe.csv: \n\n")
+for item in err_clf:
+    print(item)
