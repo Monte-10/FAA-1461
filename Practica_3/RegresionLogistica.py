984792af6bef1c994dc8da3502e42eec20519104
rtorralba <rubeninginf@gmail.com>
2022-11-20 18:50:27 +0100

    Score para RL

--- Practica_3/RegresionLogistica.py
+++ Practica_3/RegresionLogistica.py
@@ -11,7 +11,9 @@ class RegresionLogistica(Clasificador):
 
     def __init__(self) -> None:
         super().__init__()
-    
+        self.matrizConfusion = np.empty((2,2)) #¿asumo que los valores de clase siempre van a ser 0 y 1? De momento si. Inicializo con tam de matrix 2x2
+
+
     def getVectorAleatorio(self,nominalAtributos):
         lista = []
         for i in range(len(nominalAtributos)): #el vector tiene tamanio de x, es decir: sin incluir la clase
@@ -24,6 +26,45 @@ class RegresionLogistica(Clasificador):
             return ret
         except Exception:
             return 0
+    
+    def score(self,datosTest,prediccion):
+        clases = datosTest.iloc[:,-1].to_numpy().astype('int64')
+        tp = 0
+        tn = 0
+        fp = 0
+        fn = 0
+        counter = 0
+        for elem in clases:
+            # print(f'{type(elem)}  ============== {type(prediccion[counter])}')
+            # print(f'real: {elem}  ============== pred: {prediccion[counter]}')
+            if elem == prediccion[counter]:
+                if elem == 1:
+                    # print("tp + 1\n")
+                    tp += 1
+                else:
+                    # print("tn + 1\n")
+                    tn += 1
+            else:
+                if elem == 1:
+                    # print("fn + 1\n")
+                    fn += 1 
+                else:
+                    # print("fp + 1\n")
+                    fp += 1
+            counter += 1
+
+        self.matrizConfusion[0][0] = int(tp)
+        self.matrizConfusion[0][1] = int(fp)
+        self.matrizConfusion[1][0] = int(fn)
+        self.matrizConfusion[1][1] = int(tn)
+
+        self.TPR = tp / (tp+fn)
+        self.FNR = fn / (tp+fn)
+        self.FPR = fp / (fp+tn)
+        self.TNR = tn / (fp+tn)
+
+
+
 
 
     def entrenamiento(self,gradiente,num_epocas, datosTrain, nominalAtributos, diccionario):
@@ -83,6 +124,7 @@ class RegresionLogistica(Clasificador):
         #print(clasificador)
         error = 0
 
+        self.score(datosTest, clasificador)
 
         for i in range(len(clasificador)):
             #print(f'prediccion: {clasificador[i]} clase: {clases[i]}')
@@ -96,7 +138,7 @@ class RegresionLogistica(Clasificador):
         #TODO: ¿Al hacer el np.dot(w,xj) w no debe ser (wo,w) y xj (x0,xj)?
 
 
-
+    
 
     
 if __name__ == '__main__':
@@ -106,8 +148,8 @@ if __name__ == '__main__':
     gradiente = 1
     errores = []
 
-    #Si queremos normalizar:
-    #dataset.datos = ClasificadorKNN().normalize(dataset.datos)
+    # Si queremos normalizar:
+    # dataset.datos = ClasificadorKNN().normalize(dataset.datos)
 
     for j in range(50):
         validacionSimple = EstrategiaParticionado.ValidacionSimple(25,5)
@@ -121,15 +163,16 @@ if __name__ == '__main__':
             error = rl.clasificacion(w,datosTest)
             errores.append(error/len(datosTest))
     print(f'El error para pima es: {np.mean(errores)}')
+    print(f"Los valores de la matriz de confusion para este primer dataset es: \n{rl.matrizConfusion[0][0]}  {rl.matrizConfusion[0][1]}\n{rl.matrizConfusion[1][0]}  {rl.matrizConfusion[1][1]}")
 
     dataset = Datos('ConjuntosDatosP2/wdbc.csv')
-    rl = RegresionLogistica()
+    rl2 = RegresionLogistica()
     n_epocas = 10
     gradiente = 1
     errores = []
 
     #Si queremos normalizar:
-    #dataset.datos = ClasificadorKNN().normalize(dataset.datos)
+    # dataset.datos = ClasificadorKNN().normalize(dataset.datos)
     
     for j in range(50):
         validacionSimple = EstrategiaParticionado.ValidacionSimple(25,5)
@@ -138,9 +181,10 @@ if __name__ == '__main__':
 
             datosTrain = dataset.extraeDatos(validacionSimple.particiones[i].indicesTrain)
             datosTest = dataset.extraeDatos(validacionSimple.particiones[i].indicesTest) 
-            w = rl.entrenamiento(gradiente,n_epocas,datosTrain,dataset.nominalAtributos,dataset.diccionario)
+            w = rl2.entrenamiento(gradiente,n_epocas,datosTrain,dataset.nominalAtributos,dataset.diccionario)
             #print(f'W------------------------------------------------------{w}')
-            error = rl.clasificacion(w,datosTest)
+            error = rl2.clasificacion(w,datosTest)
             errores.append(error/len(datosTest))
     print(np.mean(errores))
     print(f'El error para wdbc es: {np.mean(errores)}')
+    print(f"Los valores de la matriz de confusion para este primer dataset es: \n{rl2.matrizConfusion[0][0]}  {rl2.matrizConfusion[0][1]}\n{rl2.matrizConfusion[1][0]}  {rl2.matrizConfusion[1][1]}")
